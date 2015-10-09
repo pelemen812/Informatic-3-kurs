ae67c62fdf91554535833068a390e524c5401a63
Sereja <Serejanator3000@b.com>
2015-10-09 15:51:33 +0300

    Fibonacci Number

--- main.cpp
+++ main.cpp
@@ -8,12 +8,18 @@ double sqr (double x)
     return x*x;
 }
 
+int fib(int i)
+{
+return i==1? 1: i ==2? 2: fib(i-1) + fib(i-2);
+}
+
 int main()
 {
     cout << 5*3 << ' ' <<10;
     cout << endl << "hello" << endl;
     cout << sin(1) << endl;
     cout << sqr(sqr (1.5));
+    cout << fib (10) << endl;
     return 0;
 }
 
